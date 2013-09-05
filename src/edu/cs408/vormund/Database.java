201f0f914bf97d92c94e0383154ae894f6a1f9e3
Seth Cook <cooker52@gmail.com>
2013-09-05 11:59:08 -0400

    Corrected errors in SCHEMA file and added functions to Database to install the schema and execute updates.

--- src/edu/cs408/vormund/Database.java
+++ src/edu/cs408/vormund/Database.java
@@ -1,3 +1,85 @@
 package edu.cs408.vormund;
 
-public class Database {}
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.sql.*;
+
+public class Database {
+  private static final String SCHEMA_FILE = "/edu/cs408/vormund/SCHEMA.sql";
+  private static final String DATABASE_FILE = ":resource:/edu/cs408/vormund/lib-common2.3.2.jar";
+  //private static final String DATABASE_FILE = "test.db";
+
+  private Connection conn = null;
+  private Statement stmnt = null;
+
+  public Database() {
+    try {
+      Class.forName("org.sqlite.JDBC");
+      this.conn = DriverManager.getConnection("jdbc:sqlite:" + DATABASE_FILE);
+      this.stmnt = this.conn.createStatement();
+      ResultSet rslt = this.stmnt.executeQuery("select count(name) as count from sqlite_master where type='table'");
+      if( rslt.next() ) {
+        if( rslt.getInt("count") == 0 ) {
+          this.setupNewDatabaseInstance();
+        }
+      }
+    } catch(Exception e) {
+      e.printStackTrace();
+      this.conn = null;
+    }
+  }
+
+  private void setupNewDatabaseInstance() {
+    BufferedReader br = new BufferedReader(new InputStreamReader(
+        this.getClass().getResourceAsStream(SCHEMA_FILE)));
+    String schema = " ";
+    int c;
+    boolean comment=false;
+    try {
+      while( (c=br.read()) != -1 ) {
+        if( (char)c == '\n' ) continue;
+        schema += (char)c;
+        if( (char)c == ';' ) schema += '\n';
+      }
+      br.close();
+    } catch(IOException e) {
+      return;
+    }
+    for(String query : schema.split("\n")) {
+      if( this.executeUpdate(query) < 0 ) {
+        System.out.println("Error in query: " + query);
+      }
+    }
+  }
+
+  public boolean hasConnection() { return this.conn==null; }
+
+  public int executeUpdate(String query) {
+    int result=-1;
+    try {
+      if( this.stmnt==null ) this.stmnt = this.conn.createStatement();
+      result = this.stmnt.executeUpdate(query);
+    } catch(SQLException e) {
+      System.out.println("ERROR: " + e.getMessage());
+      result=-1;
+    }
+    return result;
+  }
+
+  public void close() {
+    if( this.conn != null ) {
+      try {
+        this.conn.close();
+      } catch(Exception e) {
+      } finally {
+        this.conn = null;
+      }
+    }
+  }
+
+  /*public static void main(String[] args) {
+    Database db = new Database();
+    db.close();
+  }*/
+}
--- src/edu/cs408/vormund/SCHEMA.sql
+++ src/edu/cs408/vormund/SCHEMA.sql
@@ -14,40 +14,34 @@
 **      Samuel Marshall                                                       **
 **                                                                            **
 ==============================================================================*/
-
 DROP TABLE IF EXISTS encrypted_data;
 DROP TABLE IF EXISTS data_type;
 DROP TABLE IF EXISTS user_data;
 
-CREATE TABLE `user_data` (
-  `user_id` INTEGER NOT NULL AUTOINCREMENT,
-  `user_name` TEXT NOT NULL,
-  `password` TEXT NOT NULL,
-  PRIMARY KEY(`user_id`),
-  UNIQUE(`user_name`)
+CREATE TABLE user_data (
+  user_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  user_name TEXT NOT NULL UNIQUE,
+  name TEXT NOT NULL
+); /** I am uncertain the values that we need to store, since we'll store only one row **/
+
+CREATE TABLE data_type (
+  type_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  type_name TEXT NOT NULL UNIQUE,
+  type_value TEXT NOT NULL DEFAULT "text"
 );
 
-CREATE TABLE `data_type` (
-  `type_id` INTEGER NOT NULL AUTOINCREMENT,
-  `type_name` TEXT NOT NULL,
-  `type_value` TEXT NOT NULL DEFAULT "text",
-  PRIMARY KEY(`type_id`),
-  UNIQUE(`type_name`)
+CREATE TABLE encrypted_data (
+  data_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  category TEXT NOT NULL,
+  type_id INTEGER NOT NULL,
+  encrypted_data BLOB NOT NULL,
+  note TEXT NOT NULL,
+  UNIQUE(category, type_id),
+  FOREIGN KEY(type_id) REFERENCES data_type(type_id)
 );
 
-CREATE TABLE `encrypted_data` (
-  `data_id` INTEGER NOT NULL AUTOINCREMENT,
-  `category` TEXT NOT NULL,
-  `type_id` INTEGER NOT NULL,
-  `encrypted_data` BLOB NOT NULL,
-  `note` TEXT NOT NULL,
-  PRIMARY KEY(`data_id`),
-  UNIQUE(`category`, `type_id`),
-  FOREIGN KEY `type_id` REFERENCES `data_type`(`type_id`)
-);
-
-INSERT INTO `data_type`(`type_name`, `type_value`) VALUES('username', 'text');
-INSERT INTO `data_type`(`type_name`, `type_value`) VALUES('password', 'text');
-INSERT INTO `data_type`(`type_name`, `type_value`) VALUES('file', 'file');
-INSERT INTO `data_type`(`type_name`, `type_value`) VALUES('routing_number', 'integer');
-INSERT INTO `data_type`(`type_name`, `type_value`) VALUES('account_number', 'number');
+INSERT INTO data_type(type_name, type_value) VALUES('username', 'text');
+INSERT INTO data_type(type_name, type_value) VALUES('password', 'text');
+INSERT INTO data_type(type_name, type_value) VALUES('file', 'file');
+INSERT INTO data_type(type_name, type_value) VALUES('routing_number', 'integer');
+INSERT INTO data_type(type_name, type_value) VALUES('account_number', 'number');
